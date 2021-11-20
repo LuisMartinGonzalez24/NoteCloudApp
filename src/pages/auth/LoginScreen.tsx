9d2c53da113342e0b80def768ce6018c6ee85ffc
luis-gonzalez-latptop <luismartingt@gmail.com>
2021-11-20 07:29:59 -0400

    - First steps with firebase

--- src/pages/auth/LoginScreen.tsx
+++ src/pages/auth/LoginScreen.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { authLogin } from '../../state/actionCreators/authAction';
+import { authLogin, startLogin, loginWithGoogleProvider } from '../../state/actionCreators/authAction';
 import { useForm } from '../../hooks/useForm';
 
 const LoginScreen = () => {
 
     const dispatch = useDispatch();
     const { form, onChange } = useForm({
-        emai: 'nando@gmail.com',
+        email: 'nando@gmail.com',
         password: '123456'
     });
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        console.log(form.emai, form.password);
-        dispatch(authLogin({uid: form.password, displayName: form.emai}));
+        console.log(form.email, form.password);
+        dispatch(startLogin(form.password, form.email));
     }
-    
+
+    const hanldeGoogleLogin = () => {
+        dispatch(loginWithGoogleProvider());
+    }
+
     return (
         <>
             {/** Tittle form */}
@@ -34,8 +38,8 @@ const LoginScreen = () => {
                         type='text'
                         placeholder='Email address'
                         className='auth__input-form mb14'
-                        value= {form.emai}
-                        onChange={e => onChange('emai', e.target.value)}
+                        value={form.email}
+                        onChange={e => onChange('email', e.target.value)}
                     />
                 </div>
 
@@ -70,21 +74,21 @@ const LoginScreen = () => {
                     <span className='auth__line-span'></span>
                 </span>
 
-                <button className="btn-google mb16">
+                <button className="btn-google mb16" onClick={hanldeGoogleLogin}>
                     <img className="" src="https://i.imgur.com/arC60SB.png" alt="" />
                     <span className='ml10'>Google</span>
                 </button>
 
                 <button className="btn-facebook">
-                <svg
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 24 24" width="26" height="26"
+                    <svg
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 24 24" width="26" height="26"
                     >
-                      <path
-                        fillRule="evenodd"
-                        d="M12 2C6.477 2 2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12c0-5.523-4.477-10-10-10z"
-                      ></path>
+                        <path
+                            fillRule="evenodd"
+                            d="M12 2C6.477 2 2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12c0-5.523-4.477-10-10-10z"
+                        ></path>
                     </svg>
 
                     <span className='ml10'>Facebook</span>
--- src/state/actionCreators/authAction.ts
+++ src/state/actionCreators/authAction.ts
@@ -1,5 +1,45 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { googleProvider } from '../../firebase/firebaseConfig';
 import { Action } from '../actions';
 import { AuthActionType } from '../actionTypes';
+
+//TODO: remeber use this for typescript and read documentation -> ThunkAction<void, any, unknown, any>
+
+const auth = getAuth();
+
+const startLogin = (email: string, password: string) => {
+    return (dispatch: any) => {
+        setTimeout(() => {
+            dispatch(authLogin({ uid: password, displayName: email }))
+        }, 3500);
+    }
+}
+
+const loginWithGoogleProvider = () => {
+    return (dispatch: any) => {
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                // const credential = GoogleAuthProvider.credentialFromResult(result);
+                // const token = credential?.accessToken;
+                // The signed-in user info.
+                // const user = result.user;
+                // ...
+
+                console.log(result);
+                
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    }
+}
 
 const authLogin = (payload: { uid: string, displayName: string }): Action => (
     {
@@ -14,4 +54,4 @@ const authLogout = (): Action => (
     }
 )
 
-export { authLogin, authLogout };
+export { authLogin, authLogout, startLogin, loginWithGoogleProvider };
